6009aacb48bcbcce53bce6b3027e43075394524c
Sithum Imesh <sithum.imesh20020506@icloud.com>
2023-10-25 01:31:32 +0530

    inmemoryPos

---
+++ assignment/JS/inMemoryPos/controllers/CustomerController.js
@@ -0,0 +1,75 @@
+saveCustomer();
+
+let customerId;
+let customerName;
+let customerMobile;
+let customerCity;
+let customerStreet;
+let customerNIC;
+
+function getAllCustomerForTextFeild() {
+    customerId = S('#customerId').val();
+    customerName = S('#customerName').val();
+    customerMobile = S('#customerMobile').val();
+    customerCity = S('#customerCity').val();
+    customerStreet = S('#customerStreet').val();
+    customerNIC = S('#customerNIC').val();
+}
+
+function saveCustomer() {
+    S('#btn-save').click(function () {
+        let customerIds = S('#customerId').val();
+        if (searchExistCustomer(customerIds.trim())) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'This Customer Already Exist.'
+            });
+        } else {
+            getAllCustomerForTextFeild();
+            let newCustomer = Object.assign({}, customer);
+
+            newCustomer.id = customerId;
+            newCustomer.name = customerName;
+            newCustomer.mobile = customerMobile;
+            newCustomer.nic = customerNIC;
+            newCustomer.address = customerStreet + ", " + customerCity;
+
+            customerDB.push(newCustomer);
+
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Customer has been saved',
+                showConfirmButton: false,
+                timer: 1500
+            })
+
+            loadDataTable();
+            setDataTextFeild("","","","","","");
+        }
+    });
+}
+
+function searchExistCustomer(id) {
+    return customerDB.find(function (customer) {
+        return customer.id == id;
+    });
+}
+
+function loadDataTable() {
+    S('#tBody').empty();
+    for (var customer of customerDB) {
+        var row = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.mobile}</td><td>${customer.nic}</td></tr>`;
+        S('#tBody').append(row)
+    }
+}
+
+function setDataTextFeild(id, name, mobile, nic, city, street) {
+    S('#customerId').val(id);
+    S('#customerName').val(name);
+    S('#customerMobile').val(mobile);
+    S('#customerCity').val(nic);
+    S('#customerStreet').val(city);
+    S('#customerNIC').val(street);
+}
---
+++ assignment/JS/inMemoryPos/controllers/DashboardController.js
@@ -0,0 +1,33 @@
+var S = jQuery.noConflict();
+
+S('.customer').css('display','none');
+S('.item').css('display','none');
+S('.orders').css('display','none');
+
+S('.customer-btn').click(function (){
+    S('.dashboard').css('display','none');
+    S('.customer').css('display','block');
+    S('.item').css('display','none');
+    S('.orders').css('display','none');
+});
+
+S('.item-btn').click(function (){
+    S('.dashboard').css('display','none');
+    S('.customer').css('display','none');
+    S('.item').css('display','block');
+    S('.orders').css('display','none');
+});
+
+S('.orders-btn').click(function (){
+    S('.dashboard').css('display','none');
+    S('.customer').css('display','none');
+    S('.item').css('display','none');
+    S('.orders').css('display','block');
+});
+
+S('#imgID').click(function (){
+    S('.dashboard').css('display','block');
+    S('.customer').css('display','none');
+    S('.item').css('display','none');
+    S('.orders').css('display','none');
+});
